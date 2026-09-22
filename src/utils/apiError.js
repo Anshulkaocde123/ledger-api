75744d214656e425f79337fbf43118a8aa1272ed
Anshul Jain <anshuljain532006@gmail.com>
2026-09-22 16:04:20 +0530

    feat: add utility helpers for errors, logging, and responses

---
+++ src/utils/apiError.js
@@ -0,0 +1,38 @@
+class ApiError extends Error {
+  constructor(statusCode, message, isOperational = true, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+
+  static badRequest(msg) {
+    return new ApiError(400, msg);
+  }
+
+  static unauthorized(msg = 'Unauthorized') {
+    return new ApiError(401, msg);
+  }
+
+  static forbidden(msg = 'Forbidden') {
+    return new ApiError(403, msg);
+  }
+
+  static notFound(msg = 'Resource not found') {
+    return new ApiError(404, msg);
+  }
+
+  static unprocessableEntity(msg) {
+    return new ApiError(422, msg);
+  }
+
+  static internal(msg = 'Internal server error') {
+    return new ApiError(500, msg, false);
+  }
+}
+
+module.exports = ApiError;
